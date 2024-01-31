dd779db82291cbf1f79b9d0c4572a3b510a95be9
i.sachkov <i.sachkov@telda.ru>
2024-01-31 16:48:15 +0300

    fix(AuthPage): styles

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,7 @@
 import {ActionFunctionArgs, Form, redirect, useActionData} from 'react-router-dom';
-import React, {useEffect, useState} from 'react';
+import React, {CSSProperties, useEffect, useState} from 'react';
 import {IDeniedRequestResult, RequestResult} from '@models/auth-models.ts';
-import {TextField} from '@mui/material';
+import {Button, TextField} from '@mui/material';
 
 
 const authFormKeys = {
@@ -34,6 +34,12 @@ export const AuthAction = async ({request}: ActionFunctionArgs) => {
 const AuthPage = () => {
   const actionData = useActionData() as IDeniedRequestResult;
   const [isFormDisabled, setIsFormDisabled] = useState(false);
+  const formStyle: CSSProperties = {
+    display: 'flex',
+    margin: '1rem',
+    flexDirection: 'column',
+    gap: '1rem'
+  };
 
   useEffect(() => {
     setIsFormDisabled(false);
@@ -43,16 +49,21 @@ const AuthPage = () => {
   return (
     <div style={{width: '100%', height: '100%'}}>
       <h1 style={{textAlign: 'center', margin: '4rem 0'}}>Авторизация</h1>
-      <Form method="post" action="/auth" style={{margin: '1rem'}} onSubmit={() => setIsFormDisabled(true)}>
-        <TextField name={authFormKeys.login} id="login" label="Логин" variant="outlined" sx={{display: 'block'}}/>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Form method="post" action="/auth" style={formStyle} onSubmit={() => setIsFormDisabled(true)}>
+          <TextField name={authFormKeys.login} id="login" label="Логин" variant="outlined" sx={{display: 'block'}}/>
 
-        <TextField name={authFormKeys.password} id="password" label="Пароль" variant="outlined" sx={{display: 'block'}}/>
+          <TextField name={authFormKeys.password} id="password" label="Пароль" variant="outlined" sx={{display: 'block'}}/>
 
-        {(actionData && !actionData.isSuccess) && <p>{actionData.errorMessage}</p>}
+          {(actionData && !actionData.isSuccess) && <p>{actionData.errorMessage}</p>}
 
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Button type="submit" variant="contained" disabled={isFormDisabled}>Отправить</Button>
+          </div>
 
-        <button type="submit" disabled={isFormDisabled}>Send</button>
-      </Form>
+        </Form>
+      </div>
+
     </div>
   );
 };
