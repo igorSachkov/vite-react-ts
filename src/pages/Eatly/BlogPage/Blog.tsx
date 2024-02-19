4f9b4e56b9a942b8f8789f6be6d34ad402919627
i.sachkov <i.sachkov@telda.ru>
2024-02-19 10:04:29 +0300

    feat(BlogItem): content and styles

--- src/pages/Eatly/BlogPage/Blog.tsx
+++ src/pages/Eatly/BlogPage/Blog.tsx
@@ -1,26 +1,42 @@
 import {restPostService} from '@services/rest-service.ts';
-import {IPost} from '@models/dummy-rest-model.ts';
+import {IPost, IPostsServerAnswer} from '@models/dummy-rest-model.ts';
 import {useDispatch, useSelector} from 'react-redux';
 import {IBlogSelector, setValue} from '@redux/slices/blogSlice.ts';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import style from './Blog.module.scss';
+import {BlogItem} from '@pages/Eatly/BlogPage/BlogItem/BlogItem.tsx';
 
 
 export const Blog = () => {
+  const [isLoading, setLoading] = useState(false);
   const posts: IPost[] = useSelector((state: IBlogSelector) => state.posts.value);
   const dispatch = useDispatch();
   useEffect(() => {
     if (!posts?.length) {
+      setLoading(true);
       restPostService.getAll(12)
-        .then((answer) => {
+        .then((answer: IPostsServerAnswer) => {
           console.warn(answer);
           dispatch(setValue(answer.posts));
         })
         .catch(((err: string) => {
           console.warn(err, 'err');
-        }));
+        }))
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, []);
   return (
-    <>articles</>
+    <div className={style.sectionContainer}>
+      <h2 className={`poppins-600 ${style.title}`}>Latest <span className={'primary-text'}>Articles</span></h2>
+      <div className={style.blogContainer}>
+        {posts.map(item => <BlogItem {...item} key={item.id}/>)}
+      </div>
+      <div>
+        <button disabled={isLoading}>{'<-'}arrowLeft</button>
+        <button disabled={isLoading}>arrowRight{'->'}</button>
+      </div>
+    </div>
   );
 };
---
+++ src/pages/Eatly/BlogPage/BlogItem/BlogItem.tsx
@@ -0,0 +1,24 @@
+import style from './BlogItem.module.scss';
+import {IPost} from '@models/dummy-rest-model.ts';
+
+export const BlogItem = ({title, tags, reactions, body}: IPost) => {
+  return (
+    <div className={style.blog}>
+      <h3 className={'poppins-600'}>{title}</h3>
+      <div className={style.info}>
+        <div className={style.tags}>
+          {tags.map((tag, i) => {
+            return <span className={'poppins-400'}>#{tag}{i + 1 < tags.length && ','} </span>;
+          })}
+        </div>
+        <div>
+          <span>star</span>
+          <span className={'manrope'}>{reactions}</span>
+        </div>
+      </div>
+      <div className={style.body}>
+        <p className={'poppins-400'}>{body}</p>
+      </div>
+    </div>
+  );
+};
