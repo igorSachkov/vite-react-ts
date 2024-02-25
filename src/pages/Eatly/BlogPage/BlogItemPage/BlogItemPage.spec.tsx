3d3f74042519d602c49c1d906be02e5bfc019cb3
igorSachkov <igordadadadanet@yandex.ru>
2024-02-25 18:44:23 +0300

    feat(BlogItemPage.spec.tsx): component render test

--- src/pages/Eatly/BlogPage/BlogItemPage/BlogItemPage.spec.tsx
+++ src/pages/Eatly/BlogPage/BlogItemPage/BlogItemPage.spec.tsx
@@ -1,25 +1,87 @@
-import {cleanup, fireEvent, render} from '@testing-library/react';
+import {act, cleanup, render} from '@testing-library/react';
 import {BlogItemPage} from './BlogItemPage.tsx';
-import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Router from 'react-router-dom';
+import {restCommentsService, restPostService, restUserService} from '@services/rest-service.ts';
+import {IBlogItemParams} from '@models/blog-models.ts';
+import {IComment, ICommentsServerAnswer, IPost, IUser} from '@models/dummy-rest-model.ts';
+import style from './BlogItemPage.module.scss';
 
 afterEach(cleanup);
 
-// const mockedUsedNavigate = jest.fn();
-//
-// jest.mock("react-router-dom", () => ({
-//   ...(jest.requireActual("react-router-dom") as any),
-//   useNavigate: () => mockedUsedNavigate
-// }));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
+
+const createWrapper = () => {
+  return render(
+    <BrowserRouter>
+      <BlogItemPage/>
+    </BrowserRouter>);
+};
+
+const mockId = 'test_id';
+const mockBlogItemParams: Partial<IBlogItemParams> = {id: mockId};
+
+const mockPost: IPost = {
+  userId: 4,
+  id: 4,
+  body: 'test_body',
+  reactions: 4,
+  tags: [],
+  title: 'test_title'
+};
+const mockPostServiceAnswer: Promise<IPost> = new Promise(resolve => resolve(mockPost));
+
+const mockUserForBlog = {
+  firstName: 'test_first_name',
+  id: 4,
+  image: 'test_img_url',
+  lastName: 'test_last_name'
+} as IUser;
+const mockUserServiceAnswer: Promise<IUser> = new Promise(resolve => resolve(mockUserForBlog));
+
+const mockComments: IComment[] = [
+  {
+    body: 'test_comment_body',
+    postId: 4,
+    id: 4,
+    user: {
+      id: 5,
+      username: 'test_user_name'
+    }
+  }
+];
+const mockCommentsAnswer: ICommentsServerAnswer = {
+  comments: mockComments,
+  total: mockComments.length,
+  limit: 0,
+  skip: 0
+};
+const mockCommentsServiceAnswer: Promise<ICommentsServerAnswer> = new Promise(resolve => resolve(mockCommentsAnswer));
+
 
 describe('BlogItemPage', () => {
-  it('CheckboxWithLabel changes the text after click', () => {
-    const component = render(
-      <BrowserRouter>
-        <BlogItemPage/>
-      </BrowserRouter>
-    );
-    expect(true).toBeTruthy()
+  it('The component rendered correctly', async () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue(mockBlogItemParams);
+    jest.spyOn(restPostService, 'getItem').mockReturnValue(mockPostServiceAnswer);
+    jest.spyOn(restUserService, 'getItem').mockReturnValue(mockUserServiceAnswer);
+    jest.spyOn(restCommentsService, 'getAllCommentsByPostId').mockReturnValue(mockCommentsServiceAnswer);
+    const wrapper = await act(async () => createWrapper());
+    const commentElements = wrapper.container.getElementsByClassName(style.commentsContainer);
+
+    expect(commentElements.length).toBe(mockComments.length);
+
+
+    // expect(fetch).toHaveBeenCalledTimes(3);
+    // expect(fetch).toHaveBeenCalledWith('https://website.com/users', {
+    //   method: 'POST',
+    // });
+
+
     // const {queryByLabelText, getByLabelText, } = render(
     //   <CheckboxWithLabel labelOn="On" labelOff="Off" />,
     // );
