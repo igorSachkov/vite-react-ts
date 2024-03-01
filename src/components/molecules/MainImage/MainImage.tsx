d7afd74eb0210098bdcb707111352377a355623a
i.sachkov <i.sachkov@telda.ru>
2024-03-01 10:41:01 +0300

    ref(app): img folder change

--- src/components/molecules/MainImage/MainImage.tsx
+++ src/components/molecules/MainImage/MainImage.tsx
@@ -1,11 +1,21 @@
 import style from './MainImage.module.scss';
-import mainImg from '@assets/Group 427320327.png';
-import illustration2 from '@assets/Illustration 02.png';
-import illustration1 from '@assets/Illustration 01.png';
-import graphic from '@assets/Graph.png';
-import info from '@assets/1st.png';
+import mainImg from '@assets/mainSectionImages/Group 427320327.png';
+import illustration2 from '@assets/mainSectionImages/Illustration 02.png';
+import illustration1 from '@assets/mainSectionImages/Illustration 01.png';
+import graphic from '@assets/mainSectionImages/Graph.png';
+import {DeliveryStatus, IDeliveryStatus} from '@models/purchase-models.ts';
+import chickenHellImg from '@assets/purchaseSection/chicken-hell.png';
+import {DeliveryStatusItem} from '@components/molecules/DeliveryStatusItem/DeliveryStatusItem.tsx';
 
 export const MainImage = () => {
+  const deliveryItem: IDeliveryStatus = {
+    title: 'Chicken Hell',
+    status: DeliveryStatus.ON_THE_WAY,
+    time: '3:09 PM',
+    image: <img src={chickenHellImg} alt='Chicken hell dish'/>,
+    key: 100
+  };
+
   return (
     <div className={style.imageContainer}>
       <img className={style.rightIllustration} src={illustration1} alt="illustation"/>
@@ -13,7 +23,9 @@ export const MainImage = () => {
       <div className={style.mainImgContainer}>
         <img className={style.mainImg} src={mainImg} alt="Pure Flavour"/>
         <img className={style.graphicImg} src={graphic} alt="Graphic"/>
-        <img className={style.commentImg} src={info} alt="On the way"/>
+        <div className={style.commentImg}>
+          <DeliveryStatusItem {...deliveryItem}/>
+        </div>
 
       </div>
     </div>
